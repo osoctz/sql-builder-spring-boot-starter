5e97c561701e45924d60b8d53e8ee5ab3ecb542c
zantang <271129083@qq.com>
2021-09-27 20:07:44 +0800

    update: service&dao接口

--- src/main/java/cn/metaq/sqlbuilder/Application.java
+++ src/main/java/cn/metaq/sqlbuilder/Application.java
@@ -2,6 +2,10 @@ package cn.metaq.sqlbuilder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
@@ -9,9 +13,13 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
  */
 @EnableOpenApi
 @SpringBootApplication
+@EnableMongoRepositories(excludeFilters = {
+    @Filter(type = FilterType.ASSIGNABLE_TYPE, value = cn.metaq.data.jpa.BaseDao.class)})
+@EnableJdbcRepositories(excludeFilters = {
+    @Filter(type = FilterType.ASSIGNABLE_TYPE, value = cn.metaq.data.jpa.BaseDao.class)})
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
